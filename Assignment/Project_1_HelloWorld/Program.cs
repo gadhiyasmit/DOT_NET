4e89152e84e34fa76855eb9892f1afacabbd1f5c
gadhiyasmit <smitgadhiya9@gmail.com>
2026-06-25 17:31:39 +0530

    Assignment1

---
+++ Assignment/Project_1_HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_1_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
